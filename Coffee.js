492422830151b7ee4315ad2bc60448a6d0bbb6ed
Ryan Bird <ryanbirdca23bvt@gmail.com>
2023-09-25 17:53:24 -0700

    please

--- Coffee.js
+++ Coffee.js
@@ -251,7 +251,7 @@ function reset_game(){
 //doesn't work
 //garbage 
 //harder than implementing local storage
-//explodeii
+//explodeiii
 function progress_bar(coffee_, id){
     if (coffee_.is_progressing() == false) {}
     else{
